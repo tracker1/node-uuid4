2c7823e04eed19c7d8128055a5d42c37c28e364f
Michael J. Ryan <tracker1@gmail.com>
2014-10-14 15:33:55 -0700

    fix fat-finger issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var crypto = require('crypto')
     ,uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
     ;
 
-exports = module.exports = genUuid;
+exports = module.exports = genUUID;
 exports.isUUID = isUUID;
 
 
